bfa7fa89996e840394a81293cd7f45ddcde5f12e
Your Name <niktjawalla922@gmail.com>
2023-05-23 15:24:32 +0200

    add creation on drag

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
+var isCreatingMainDiv = false;
+
+
+
+function startCreatingMainDiv(event) {
+  isCreatingMainDiv = true;
+  createMainDiv(event);
+}
+
+function stopCreatingMainDiv() {
+  isCreatingMainDiv = false;
+}
+
+function ripple(event){
+  if(isCreatingMainDiv){
+    createMainDiv(event)
+  }
+}
+
 function createMainDiv(event) {
-    // Get the click coordinates
-    var x = event.clientX;
-    var y = event.clientY;
     
-    // Create the main div element
-    var mainDiv = document.createElement('div');
-    mainDiv.classList.add('main');
+      // Get the click coordinates
+      var x = event.clientX;
+      var y = event.clientY;
     
-    // Set the position of the main div to the click coordinates
-    mainDiv.style.left = x + 'px';
-    mainDiv.style.top = y + 'px';
+      // Create the main div element
+      var mainDiv = document.createElement('div');
+      mainDiv.classList.add('main');
     
-    // Create the wave divs
-    for (var i = 1; i <= 3; i++) {
+      // Set the position of the main div to the click coordinates
+      mainDiv.style.left = x + 'px';
+      mainDiv.style.top = y + 'px';
+    
+      // Create the wave divs
+      for (var i = 1; i <= 3; i++) {
       var waveDiv = document.createElement('div');
       waveDiv.classList.add('wave', 'wave' + i);
       
-    // Give waves randomized shape
+      // Give waves randomized shape
 
       waveDiv.style.borderTopLeftRadius = getRandomPercentage(80,100);
       waveDiv.style.borderTopRightRadius = getRandomPercentage(80,100);
@@ -26,14 +46,19 @@ function createMainDiv(event) {
       function getRandomPercentage(min, max) {
         var randomPercentage = Math.floor(Math.random() * (max - min + 1) + min);
         return randomPercentage + '%';
-    }
+      }
 
-    // Append Wave to main Div
+      // Append Wave to main Div
 
       mainDiv.appendChild(waveDiv);
-    }
+      }
     
-    // Append the main div to the container
-    var container = document.querySelector('.container');
-    container.appendChild(mainDiv);
+      // Append the main div to the container
+      var container = document.querySelector('.container');
+      container.appendChild(mainDiv);
+
+        
   }
+
+
+  
